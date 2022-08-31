13b467644281c69cfbdfaf9529e5481e0f48a1e1
Artur <artur.onoriichuk@gmail.com>
2022-09-01 00:33:56 +0300

    feat: change indicator test

---
+++ client/src/tests/ChangeIndicator/ChangeIndicator.test.js
@@ -0,0 +1,19 @@
+import {renderWithRedux} from "../renderWithRedux";
+import {screen} from "@testing-library/react";
+import React from "react";
+import ChangeIndicator from "../../components/Home/Tickers/ChangeIndicator";
+
+describe("change indicator component", () => {
+
+    test("should take 'Up' prop and render 'Up' indicator", () => {
+        let indicator = "Up"
+        renderWithRedux(<ChangeIndicator indicator={indicator}/>);
+        expect(screen.getByAltText("up")).toBeInTheDocument();
+    })
+
+    test("should take 'Down' prop and render 'Down' indicator", () => {
+        let indicator = "Down"
+        renderWithRedux(<ChangeIndicator indicator={indicator}/>);
+        expect(screen.getByAltText("down")).toBeInTheDocument();
+    })
+})
